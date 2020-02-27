47797f9fb1800437dfeeadf9d7a245b012afe878
karlcar <498073785@qq.com>
2020-02-27 18:28:38 +0800

    P30 分类管理3 -3 1、编写查找并去到的编辑页面（TypeController里面的4） 2、编辑提交功能（TypeController里面的5，types-input.html里面增加th:action和input）！！！（编辑之后使用新增提交功能不成功） 3、删除功能（TypeController里面的6）

--- src/main/java/springBoot/controller/admin/TypeController.java
+++ src/main/java/springBoot/controller/admin/TypeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -59,6 +60,42 @@ public class TypeController {
 		}else {
 			attributes.addFlashAttribute("message", "新增失败");
 		}
+		return "redirect:/admin/types";
+	}
+	
+	//4、查找到并去到要修改的页面
+	@GetMapping("/types/{id}/input")
+	public String editInput(@PathVariable Long id,Model model) {
+		model.addAttribute("type", typeService.getType(id));
+		return "admin/types-input";
+	}
+	
+	//5、编辑提交功能
+	@PostMapping("/types/{id}")
+	public String post(@Valid Type type,BindingResult result,@PathVariable Long id, RedirectAttributes attributes) { //BindingResult接收校验的结果,@Valid必须和BindingResult连在一起
+		Type t1 = typeService.getTypeByName(type.getName());
+		if(t1 != null) {
+			result.rejectValue("name", "nameError", "不能添加重复分类");
+		}
+		
+		if(result.hasErrors()) {
+			return "admin/types-input";
+		}
+		
+		Type t = typeService.updateType(id, type);
+		if(t != null) { 
+			attributes.addFlashAttribute("message", "更新成功");
+		}else {
+			attributes.addFlashAttribute("message", "更新失败");
+		}
+		return "redirect:/admin/types";
+	}	
+	
+	//6、删除功能
+	@GetMapping("/types/{id}/delete")
+	public String delete(@PathVariable Long id,RedirectAttributes attributes) {
+		typeService.deleteType(id);
+		attributes.addFlashAttribute("message", "删除成功");
 		return "redirect:/admin/types";
 	}
 	
